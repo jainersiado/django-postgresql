666c39bd778df281e79b2446c9cbb4106d778afb
jainer_siado <jainersiado@hotmail.com>
2023-07-01 19:32:12 -0500

    second commit

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.pos',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
